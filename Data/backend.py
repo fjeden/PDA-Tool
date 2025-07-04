5b37fd463d7bcb8b3dfc7b6a81578b4c942033ab
fjeden <116516317+fjeden@users.noreply.github.com>
2025-07-04 17:19:18 +0200

    Add files via upload

---
+++ Data/backend.py
@@ -0,0 +1,154 @@
+Betrachtungsebene = ["Bitte wählen Sie einen Wert aus...", "Produkt bzw. Bausatz", "Baustoff", "Bauteil", "Anlage"]
+
+Bauwerkskategorien = ["Bitte wählen Sie einen Wert aus ...",'Wohnen – Einfache Behelfsbauten für vorübergehende Nutzung', 'Wohnen – Einfache Wohnbauten mit gemeinschaftlichen Sanitär- und Kücheneinrichtungen', 
+                      'Wohnen – Einfamilienhäuser, Wohnhäuser oder Hausgruppen in verdichteter Bauweise', 
+                      'Wohnen – Wohnheime, Gemeinschaftsunterkünfte, Jugendherbergen, -freizeitzentren, -stätten', 
+                      'Ausbildung/Wissenschaft/Forschung – Offene Pausen-, Spielhallen', 'Ausbildung/Wissenschaft/Forschung – Studentenhäuser', 
+                      'Ausbildung/Wissenschaft/Forschung – Schulen mit durchschnittlichen Planungsanforderungen, zum Beispiel Grundschulen, weiterführende Schulen und Berufsschulen', 
+                      'Ausbildung/Wissenschaft/Forschung – Schulen mit hohen Planungsanforderungen, Bildungszentren, Hochschulen, Universitäten, Akademien', 
+                      'Ausbildung/Wissenschaft/Forschung – Hörsaal-, Kongresszentren', 'Ausbildung/Wissenschaft/Forschung – Labor- oder Institutsgebäude', 
+                      'Büro/Verwaltung/Staat/Kommune – Büro-, Verwaltungsgebäude', 'Büro/Verwaltung/Staat/Kommune – Wirtschaftsgebäude, Bauhöfe', 
+                      'Büro/Verwaltung/Staat/Kommune – Parlaments-, Gerichtsgebäude', 'Büro/Verwaltung/Staat/Kommune – Bauten für den Strafvollzug', 
+                      'Büro/Verwaltung/Staat/Kommune – Feuerwachen, Rettungsstationen', 'Büro/Verwaltung/Staat/Kommune – Sparkassen- oder Bankfilialen', 
+                      'Büro/Verwaltung/Staat/Kommune – Büchereien, Bibliotheken, Archive', 'Gesundheit/Betreuung – Liege- oder Wandelhallen', 
+                      'Gesundheit/Betreuung – Kindergärten, Kinderhorte', 'Gesundheit/Betreuung – Jugendzentren, Jugendfreizeitstätten', 
+                      'Gesundheit/Betreuung – Betreuungseinrichtungen, Altentagesstätten', 
+                      'Gesundheit/Betreuung – Pflegeheime oder Bettenhäuser, ohne oder mit medizinisch-technischer Einrichtungen,', 
+                      'Gesundheit/Betreuung – Unfall-, Sanitätswachen, Ambulatorien', 
+                      'Gesundheit/Betreuung – Therapie- oder Rehabilitations-Einrichtungen, Gebäude für Erholung, Kur oder Genesung', 
+                      'Gesundheit/Betreuung – Hilfskrankenhäuser', 
+                      'Gesundheit/Betreuung – Krankenhäuser der Versorgungsstufe I oder II, Krankenhäuser besonderer Zweckbestimmung', 
+                      'Gesundheit/Betreuung – Krankenhäuser der Versorgungsstufe III, Universitätskliniken', 
+                      'Handel und Verkauf/Gastgewerbe – Einfache Verkaufslager, Verkaufsstände, Kioske', 
+                      'Handel und Verkauf/Gastgewerbe – Ladenbauten, Discounter, Einkaufszentren, Märkte, Messehallen', 
+                      'Handel und Verkauf/Gastgewerbe – Gebäude für Gastronomie, Kantinen oder Mensen', 
+                      'Handel und Verkauf/Gastgewerbe – Großküchen, mit oder ohne Speiseräume', 
+                      'Handel und Verkauf/Gastgewerbe – Pensionen, Hotels', 
+                      'Freizeit/Sport – Einfache Tribünenbauten', 
+                      'Freizeit/Sport – Bootshäuser', 
+                      'Freizeit/Sport – Turn- oder Sportgebäude', 
+                      'Freizeit/Sport – Mehrzweckhallen, Hallenschwimmbäder, Großsportstätten', 
+                      'Gewerbe/Industrie/Landwirtschaft – Einfache Landwirtschaftliche Gebäude, zum Beispiel Feldscheunen, Einstellhallen', 
+                      'Gewerbe/Industrie/Landwirtschaft – Landwirtschaftliche Betriebsgebäude, Stallanlagen', 
+                      'Gewerbe/Industrie/Landwirtschaft – Gewächshäuser für die Produktion', 
+                      'Gewerbe/Industrie/Landwirtschaft – Einfache geschlossene, eingeschossige Hallen, Werkstätten', 
+                      'Gewerbe/Industrie/Landwirtschaft – Spezielle Lagergebäude, zum Beispiel Kühlhäuser', 
+                      'Gewerbe/Industrie/Landwirtschaft – Werkstätten, Fertigungsgebäude des Handwerks oder der Industrie', 
+                      'Gewerbe/Industrie/Landwirtschaft – Produktionsgebäude der Industrie', 
+                      'Infrastruktur – Offene Verbindungsgänge, Überdachungen, zum Beispiel Wetterschutzhäuser, Carports', 
+                      'Infrastruktur – Einfache Garagenbauten', 'Infrastruktur – Parkhäuser, -garagen, Tiefgaragen, jeweils mit integrierten weiteren Nutzungsarten', 
+                      'Infrastruktur – Bahnhöfe oder Stationen verschiedener öffentlicher Verkehrsmittel', 
+                      'Infrastruktur – Flughäfen', 
+                      'Infrastruktur – Energieversorgungszentralen, Kraftwerksgebäude, Großkraftwerke', 
+                      'Kultur-/Sakralbauten – Pavillons für kulturelle Zwecke', 
+                      'Kultur-/Sakralbauten – Bürger-, Gemeindezentren, Kultur-/Sakralbauten, Kirchen', 
+                      'Kultur-/Sakralbauten – Mehrzweckhallen für religiöse oder kulturelle Zwecke', 
+                      'Kultur-/Sakralbauten – Ausstellungsgebäude, Lichtspielhäuser', 
+                      'Kultur-/Sakralbauten – Museen', 
+                      'Kultur-/Sakralbauten – Theater-, Opern-, Konzertgebäude', 'Kultur-/Sakralbauten – Studiogebäude für Rundfunk oder Fernsehen']
+
+
+Gebäudeklassen = ["Bitte wählen Sie einen Wert aus ...","Gebäudeklasse 1a: Freistehende Gebäude mit einer Höhe bis zu 7 m und nicht mehr als zwei Nutzungseinheiten von insgesamt nicht mehr als 400 m²", 
+"Gebäudeklasse 1b: Freistehende land- oder forstwirtschaftlich genutzte Gebäude",
+"Gebäudeklasse 2: Gebäude mit einer Höhe bis zu 7 m und nicht mehr als zwei Nutzungseinheiten von insgesamt nicht mehr als 400 m²",
+"Gebäudeklasse 3: Sonstige Gebäude mit einer Höhe bis zu 7 m",
+"Gebäudeklasse 4: Gebäude mit einer Höhe bis zu 13 m und Nutzungseinheiten mit jeweils nicht mehr als 400 m²",
+"Gebäudeklasse 5: Sonstige Gebäude einschließlich unterirdischer Gebäude"    
+]
+
+Bauweise = ["Bitte wählen Sie einen Wert aus ...",'Andere / Misch-Bauweise', 'Stahl Elementbau', 'Stahl Leichtbau',
+             'Stahlbeton Elementbau', 'Stahlbeton Leichtbau', 'Stahlbeton Massivbau',
+             'Stahlbeton Raumtragwerk', 'Stahlbeton Skelettbau', 'Stahlbeton Verbundbau',
+             'Stahl-Stahlbeton-Hybridbau', 'Mauerwerk Kalksandstein', 'Mauerwerk Leichtbeton',
+             'Mauerwerk Massivbau', 'Mauerwerk Planziegel', 'Mauerwerk Porenbeton', 'Mauerwerk-Stahlbeton-Hybridbau',
+             'Modulbauweise', 'Holz Blockbau', 'Holz Elementbau', 'Holz Fachwerkbau', 'Holz Gemischt-Bauweise',
+             'Holz Massivbau', 'Holz Tafel-/Rahmen-/Ständerbau', 'Holz-Beton-Verbundbau', 'Holz-Stahlbeton-Hybridbau',
+             'Pavillon/Messestand'
+             ]
+
+DIN276klassifizierung =["Bitte wählen Sie einen Wert aus ...",
+                        '320 Gründung/Unterbau', '330 Außenwände/Vertikale Baukonstruktionen, außen',
+                        '340 Innenwände/Vertikale Baukonstruktionen, innen', '350 Decken/Horizontale Baukonstruktionen',
+                        '360 Dächer', '370 Infrastrukturanlagen', '380 Baukonstruktive Einbauten',
+                        '410 Abwasser-, Wasser-, Gasanlagen', '420 Wärmeversorgungsanlagen',
+                        '430 Raumlufttechnische Anlagen', '440 Elektrische Anlagen',
+                        '450 Kommunikations-, sicherheits- und informationstechnische Anlagen',
+                        '460 Förderanlagen', '470 Nutzungsspezifische und verfahrenstechnische Anlagen',
+                        '480 Gebäude- und Anlagenautomation', '520 Gründung, Unterbau',
+                        '530 Oberbau, Deckschichten', '540 Baukonstruktionen', '550 Technische Anlagen',
+                        '560 Einbauten in Außenanlagen und Freiflächen', '610 Allgemeine Ausstattung',
+                        '620 Besondere Ausstattung', '630 Informationstechnische Ausstattung', '640 Künstlerische Ausstattung', '690 Sonstige Ausstattung'
+]
+
+Abfallschlüsselnummer =["Bitte wählen Sie einen Wert aus ...",
+    '16 02 09* Transformatoren und Kondensatoren, die PCB enthalten',
+    '16 02 10* gebrauchte Geräte, die PCB enthalten oder damit verunreinigt sind, mit Ausnahme derjenigen, die unter 16 02 09 fallen',
+    '16 02 11* gebrauchte Geräte, die teil- und vollhalogenierte Fluorchlorkohlenwasserstoffe enthalten', 
+    '16 02 12* gebrauchte Geräte, die freies Asbest enthalten', 
+    '16 02 13* gefährliche Bestandteile enthaltende gebrauchte Geräte mit Ausnahme derjenigen, die unter 16 02 09 bis 16 02 12 fallen', 
+    '16 02 14 gebrauchte Geräte mit Ausnahme derjenigen, die unter 16 02 09 bis 16 02 13 fallen', 
+    '16 02 15* aus gebrauchten Geräten entfernte gefährliche Bestandteile',
+    '16 02 16 aus gebrauchten Geräten entfernte Bestandteile mit Ausnahme derjenigen, die unter 16 02 15 fallen', '17 01 01 Beton', '17 01 02 Ziegel', 
+    '17 01 03 Fliesen, Ziegel und Keramik', '17 01 06* Gemische aus oder getrennte Fraktionen von Beton, Ziegeln, Fliesen und Keramik, die gefährliche Stoffe enthalten', 
+    '17 01 07 Gemische aus Beton, Ziegeln, Fliesen und Keramik mit Ausnahme derjenigen, die unter 17 01 06 fallen(Bauschutt)', '17 02 01 Holz', '17 02 02 Glas', '17 02 03 Kunststoff', 
+    '17 02 04* Glas, Kunststoff und Holz, die gefährliche Stoffe enthalten oder durch gefährliche Stoffe verunreinigt sind', '17 03 01* kohlenteerhaltige Bitumengemische (Asphalt',
+    '17 03 02 Bitumengemische mit Ausnahme derjenigen, die unter 17 03 01 fallen (Asphalt)', '17 03 03* Kohlenteer und teerhaltige Produkte', '17 04 01 Kupfer, Bronze, Messing',
+    '17 04 02 Aluminium', '17 04 03 Blei', '17 04 04 Zink', '17 04 05 Eisen und Stahl', '17 04 06 Zinn', '17 04 07 gemischte Metalle',
+    '17 04 09* Metallabfälle, die durch gefährliche Stoffe verunreinigt sind', '17 04 10* Kabel, die Öl, Kohlenteer oder andere gefährliche Stoffe enthalten',
+    '17 04 11 Kabel mit Ausnahme derjenigen, die unter 17 04 10 fallen', '17 05 03* Boden und Steine, die gefährliche Stoffe enthalten', 
+    '17 05 04 Boden und Steine mit Ausnahme derjenigen, die unter 17 05 03 fallen', '17 05 05* Baggergut, das gefährliche Stoffe enthält', 
+    '17 05 06 Baggergut mit Ausnahme desjenigen, das unter 17 05 05 fällt', '17 05 07* Gleisschotter, der gefährliche Stoffe enthält', 
+    '17 05 08 Gleisschotter mit Ausnahme desjenigen, der unter 17 05 07 fällt', '17 06 01* Dämmmaterial, das Asbest enthält', 
+    '17 06 03* anderes Dämmmaterial, das aus gefährlichen Stoffen besteht oder solche Stoffe enthält', 
+    '17 06 04 Dämmmaterial mit Ausnahme desjenigen, das unter 17 06 01 und 17 06 03 fällt', '17 06 05* asbesthaltige Baustoffe', 
+    '17 08 01* Baustoffe auf Gipsbasis, die durch gefährliche Stoffe verunreinigt sind', '17 08 02 Baustoffe auf Gipsbasis mit Ausnahme derjenigen, die unter 17 08 01 fallen', 
+    '17 09 01* Bau- und Abbruchabfälle, die Quecksilber enthalten', 
+    '17 09 02* Bau- und Abbruchabfälle, die PCB enthalten (z. B. PCB-haltige Dichtungsmassen, PCB-haltige Bodenbeläge auf Harzbasis, PCB-haltige Isolierverglasungen, PCB-haltige Kondensatoren)',
+    '17 09 03* sonstige Bau- und Abbruchabfälle (einschliesslich gemischte Abfälle), die gefährliche Stoffe enthalten', 
+    '17 09 04 gemischte Bau- und Abbruchabfälle mit Ausnahme derjenigen, die unter 17 09 01, 17 09 02 und 17 09 03 fallen', 
+    '20 01 21* Leuchtstoffröhren und andere quecksilberhaltige Abfälle', '20 01 23* gebrauchte Geräte, die Fluorchlorkohlenwasserstoffe enthalten', 
+    '20 01 35* gebrauchte elektrische und elektronische Geräte, die gefährliche Bauteile enthalten, mit Ausnahme derjenigen, die unter 20 01 21 und 20 01 23 fallen', 
+    '20 01 36 gebrauchte elektrische und elektronische Geräte mit Ausnahme derjenigen, die unter 20 01 21, 20 01 23 und 20 01 35 fallen']
+
+    
+Materialien = ["Bitte wählen Sie einen Wert aus ...",
+               'Beton', 'Ziegel', 'Fliesen', 'Keramik', 'Holz', 'Glas', 'Kunststoff', 'Bitumengemische',
+               'Kupfer/Bronze/Messing', 'Aluminium', 'Eisen/Stahl', 'Andere Metalle', 'Kabel', 'Erde und Steine', 'Baggergut',
+               'Gleisschotter', 'Dämmstoffe', 'Asbesthaltige Materialien', 'Materialien auf Gipsbasis', 'Elektro- und Elektronikaltgeräte', 'Anderes']    
+
+Zugänglichkeit = ["Bitte wählen Sie einen Wert aus ...",'Frei zugänglich ohne zusätzliche Maßnahmen', 'Zugänglich mit zusätzlichen Aktionen, die keinen Schaden verursachen', 'Zugänglich mit zusätzlichen Maßnahmen und mit vollständig reparierbaren Schäden',
+                  'Zugänglich mit zusätzlichen Maßnahmen und mit teilweise reparierbaren Schäden', 'Nicht zugänglich - irreparable Schäden am Produkt oder denumliegenden Produkten']
+
+Verbindungart_grob = ["Bitte wählen Sie einen Wert aus ...", 'Lose / Klickverbindung', 'Gesteckt / Geschraubt', 
+                    'Festverbaut (Rückbauaufwand: mittel; Schadensart: überwiegend zerstörungsfrei lösbar)', 'Festverbaut (Rückbauaufwand: hoch; Schadensart: reparable Schäden)',
+                    'Festverbaut (Rückbauaufwand: extrem aufwändig; Schadensart: irreparable Schäden)', 'Nicht bewertbar bzw. nicht demontagefähig']
+
+Schadstoffverdacht = ["Bitte wählen Sie einen Wert aus ...", 'Keine Antwort', 'Inertabfall', 'Nicht-schädlich', 'Schädlich']
+
+Bauprodukt_Funktion = ["Bitte wählen Sie einen Wert aus ...", 'Tragwerk', 'Gebäudehülle', 'Gebäudetechnik', 'Ausstattung']
+
+Anschlussnutzungspotenzial = ["Bitte wählen Sie einen Wert aus ...", 'Hochwertige Anschlussnutzung', 'Niederwertige Anschlussnutzung', 'Keine abschließende Bewertung möglich']
+
+Verbindungsart = ["Bitte wählen Sie einen Wert aus ...", 'Lose / Klickverbindung', 'Gesteckt / Geschraubt', 
+                    'Festverbaut (Rückbauaufwand: mittel; Schadensart: überwiegend zerstörungsfrei lösbar)', 'Festverbaut (Rückbauaufwand: hoch; Schadensart: reparable Schäden)',
+                    'Festverbaut (Rückbauaufwand: extrem aufwändig; Schadensart: irreparable Schäden)', 'Nicht bewertbar bzw. nicht demontagefähig']
+
+Potenzielle_Anschlussnutzung = ["Bitte wählen Sie einen Wert aus ...", 'Wiederverwendung', 'Vorbereitung zur Wiederverwendung', 'Recycling', 'Sonstige Verwertung', 'Beseitigung']
+
+levels_Anschlussnutzung = ["Bitte wählen Sie einen Wert aus ...", 'Element für die Wiederverwendung vor Ort vorbereiten',
+                           'Element für die Wiederverwendung außerhalb des Standorts vorbereiten', 'Material für die Wiederverwendung vor Ort vorbereiten',
+                           'Vorbereiten von Material für die Wiederverwendung außerhalb des Standorts', 'Wiederverwendung/Aufarbeitung von Elektro- und Elektronik-Altgeräten',
+                           'Onsite-Recycling von reinen Abbruchabfallströmen', 'Offsite-Recycling von reinen Abbruchabfallströmen', 'Vor-Ort-Recycling von gemischten Abbruchabfällen',
+                           'Offsite-Recycling von gemischten Abbruchabfallströmen', 'Onsite-Recycling von kontaminierten Böden (In-situ-Sanierung)', 
+                           'Offsite-Recycling von kontaminierten Böden (Ex-situ-Sanierung)', 'Elektro- und Elektronik-Altgeräte Anlage zur Verwertung',
+                           'Materialverwertung vor Ort als Aufschüttung/Landschaftsbau', 'Offsite-Materialrückgewinnung als Aufschüttung/Landschaftsbau',
+                           'Energetische Verwertung in einer Abfallverbrennungsanlage', 'Thermische Zerstörung durch Verbrennung', 'Inertabfalldeponie',
+                           'Deponie für nicht gefährliche Abfälle', 'Stabile, nicht reaktive Deponie für gefährliche Abfälle', 'Deponie für gefährliche Abfälle']
+
+
+levels_Rückbaumaßnahme = ["Bitte wählen Sie einen Wert aus ...", 'Vorbereiten des Elements für die Wiederverwendung vor Ort', 
+                          'Dekontaminierung (die gezielte Entfernung von gefährlichen Materialien oder Stoffen)',
+                          'Entfernen von hochwertigen Materialien oder Elementen/Bauteilen (für den Verkauf/die Wiederverwendung an einem anderen Ort)', 
+                          'Rückbau von Elementen (für den Verkauf/die Wiederverwendung an einem anderen Ort)', 'Abriss (einschließlich selektivem Abriss)']
+
+
